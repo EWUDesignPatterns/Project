be5ff6e8c1874029b306f0947de3e2182ca6ec7f
Andrew Canfield <Andrew@thePOG.net>
2012-08-13 18:04:37 -0700

    Implemented monster character.

---
+++ src/gameObjects/MonsterFactory.java
@@ -0,0 +1,52 @@
+package gameObjects;
+
+import gameObjects.Race.enemies.*;
+import gameObjects.Class.enemyClasses.*;
+import java.util.*;
+
+public class MonsterFactory {
+    
+  public MonsterFactory() {
+  }
+
+  public ICharacter generate() {
+	 ICharacter monster = null;
+	 Random rand = new Random();
+	 int monster_race = rand.nextInt(4);
+	 int monster_class = rand.nextInt(4);
+	 
+	 switch (monster_race) {
+	 case 1:
+		 monster = new Kobold();
+		 break;
+	 case 2:
+		 monster = new Ogre();
+		 break;
+	 case 3:
+		 monster = new Salamander();
+		 break;
+	 case 4:
+		 monster = new Troll();
+		 break;	 
+	 }
+	 
+	 switch (monster_class) {
+	 case 1: 
+		 monster = new Chieftain(monster);
+		 break;
+	 case 2:
+		 monster = new Flaming(monster);
+		 break;
+	 case 3:
+		 monster = new Poison(monster);
+		 break;
+	 case 4:
+		 monster = new WitchDoctor(monster);
+		 break;
+	 }
+	 
+	 return monster;
+  }
+  
+
+}
