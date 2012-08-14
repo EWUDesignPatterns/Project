c5c1507133b4e6ed5f34303d6ef21e2a9d2a98cf
Andrew Canfield <Andrew@thePOG.net>
2012-08-13 18:53:51 -0700

    Implemented ItemFactory

---
+++ src/gameObjects/ItemFactory.java
@@ -0,0 +1,127 @@
+package gameObjects;
+
+import java.util.*;
+
+import gameObjects.items.*;
+import gameObjects.items.Armors.*;
+import gameObjects.items.Weapons.*;
+import gameObjects.items.Potions.*;
+
+public class ItemFactory {
+
+	
+	public static IItem generate() {
+	  IItem item = null;
+	  Random rand = new Random();
+	  int itemType = rand.nextInt(3) + 1; 
+	  
+	  switch (itemType) {
+	  case 1:
+		item = (IItem) generateArmor();
+		break;
+	  case 2:
+		item = (IItem) generateWeapon();
+		break;
+	  case 3:
+	    item = (IItem) generatePotion();
+	    break;
+	  }
+	  
+	  return item;
+	}
+	
+	public static IArmor generateArmor() {
+	  Random rand = new Random();
+	  IArmor armor = null;
+	  int armorType = rand.nextInt(7) + 1;
+	  
+	  switch (armorType) {
+	  case 1:
+		armor = new Chainmail();
+		break;
+	  case 2:
+		armor = new Clothes();
+		break;
+	  case 3:
+		armor = new GlassArmor();
+		break;
+	  case 4:
+		armor = new LeatherArmor();
+		break;
+	  case 5:
+		armor = new LoinCloth();
+		break;
+	  case 6:
+		armor = new PlateArmor();
+		break;
+	  case 7:
+		armor = new WizardRobe();
+		break;
+	  }
+	  
+	  
+	  return armor;
+	  
+	}
+	
+	public static IWeapon generateWeapon() {
+		Random rand = new Random();
+		IWeapon weapon = null;
+		int weaponType = rand.nextInt(6) + 1;
+
+		  switch (weaponType) {
+		  case 1:
+			weapon = new BastardSword();
+			break;
+		  case 2:
+			weapon = new Broadsword();
+			break;
+		  case 3:
+			weapon = new Katana();
+			break;
+		  case 4:
+			weapon = new StaffOfMana();
+			break;
+		  case 5:
+			weapon = new TwinDaggers();
+			break;
+		  case 6:
+			weapon = new WoodenSword();
+			break;
+		  }
+		
+		
+		return weapon;
+	}
+	
+	public static IPotion generatePotion() {
+		  Random rand = new Random();
+		  IPotion potion = null;
+		  int potionType = rand.nextInt(6) + 1;
+		  
+		  switch (potionType) {
+		  case 1:
+			potion = new CureAllPotion();
+			break;
+		  case 2:
+			potion = new MajorHealthPotion();
+			break;
+		  case 3:
+			potion = new MinorDefensePotion();
+			break;
+		  case 4:
+			potion = new MinorHealthPotion();
+			break;
+		  case 5:
+			potion = new StandardHealthPotion();
+			break;
+		  case 6:
+			potion = new UltraHealthPotion();
+			break;
+		  }
+		  
+		  
+		  return potion;
+		  
+		}
+}
